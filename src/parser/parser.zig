ffe14b81048d42a6b8b7244f54d617311bc6b208
Airbus5717 <Abdussamadf350@gmail.com>
2021-09-17 14:09:35 +0300

    remove output.txt

--- src/parser/parser.zig
+++ src/parser/parser.zig
@@ -7,3 +7,19 @@ const TokenType = @import("../lexer/tokens.zig").TokenType;
 const Lexer = @import("../lexer/lexer.zig").Lexer;
 const config = @import("../config.zig");
 const log = @import("../log.zig");
+
+const BinaryExpr = struct {
+    left: Token,
+    op: Token,
+    right: Token,
+};
+
+const GroupExpr = struct {};
+const LiteralExpr = struct {};
+
+pub fn parse(lexer: *Lexer) !void {
+    for (lexer.tkns.items) |item, index| {
+        _ = item;
+        _ = index;
+    }
+}
