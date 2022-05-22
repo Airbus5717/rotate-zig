bd4d911788d5c09eefc1c33398fab8804c4b6fb1
Airbus5717 <abdussamadf350@gmail.com>
2022-05-22 22:49:40 +0300

    convert to markdown

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# Rotate Programming Language (Zig implementation)
+
+- check `docs/index.org` for documentation
+
+## requirements
+- [x] Zig compiler
--- src/main.zig
+++ src/main.zig
@@ -4,11 +4,16 @@ const Lexer = @import("./frontend/Lexer.zig");
 const Token_Type = Lexer.Token_Type;
 const reader = @import("file.zig");
 
-pub fn main() !void {
+pub fn compile() !void {
     var allocator = std.heap.raw_c_allocator;
-    _ = allocator;
 
     const file = try reader.readFile("main.vr", &allocator);
     var lexer = Lexer.init(file, allocator);
     try lexer.lex();
+}
+
+pub fn main() void {
+    compile() catch |err| {
+        std.log.err("{s}", .{@errorName(err)});
+    };
 }
